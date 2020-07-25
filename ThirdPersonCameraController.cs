fc402c8979ddccb4435fd421ae605108ca177bcc
Lorenzo <68746085+Lhenzo@users.noreply.github.com>
2020-07-25 19:43:42 +0200

    Initial Build for Controls and Camera

---
+++ ThirdPersonCameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+    public float RotationSpeed = 1;
+    public Transform Target, Player;
+    float mouseHorizontal, mouseVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseHorizontal += Input.GetAxis("Mouse X") * RotationSpeed;
+        mouseVertical -= Input.GetAxis("Mouse Y") * RotationSpeed;
+        //Pour éviter que la caméra flip : 
+        mouseVertical = Mathf.Clamp(mouseVertical, -80, 100);
+
+        transform.LookAt(Target);
+
+        if (Input.GetMouseButton(1)) //clique droit enfoncé
+        {
+            Target.rotation = Quaternion.Euler(mouseVertical, mouseHorizontal, 0);
+        }
+        else
+        {
+            Target.rotation = Quaternion.Euler(mouseVertical, mouseHorizontal, 0);
+            Player.rotation = Quaternion.Euler(0, mouseHorizontal, 0);
+        }
+        
+    }
+
+}
---
+++ ThirdPersonCharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public float Speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+
+
+    void PlayerMovement()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(horizontalMovement, 0f, verticalMovement).normalized * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+        
+    }
+}
